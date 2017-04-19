752c8361dfa6a1cbfc67446f56c1566c87f9f9d3
ghofer <gabor.hofer@frontsoft.hu>
2017-04-19 14:59:31 +0200

    Develpoment v.0.1

--- cg/consoleWindowManager.cpp
+++ cg/consoleWindowManager.cpp
@@ -1,5 +1,6 @@
-﻿#include <iostream>
+﻿//http://en.cppreference.com/w/
 
+#include <iostream>
 #include "consoleWindowManager.h"
 
 using namespace std;
@@ -8,6 +9,8 @@ ConsoleWindowManager::ConsoleWindowManager() {
 	//cout << "Object is being created" << endl;
 	mHConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	init(mScreenWidth, mScreenHeight);
+	initLayer();
+	initGame();
 }
 
 ConsoleWindowManager::~ConsoleWindowManager() {
@@ -29,20 +32,131 @@ void ConsoleWindowManager::init(int& mScreenWidth, int& mScreenHeight)
 	else {
 		MoveWindow(mhWnd, 0, 0, CONSOLE_WINDOW_WIDTH, CONSOLE_WINDOW_HEIGHT, TRUE);
 	}
-	SetConsoleTitle( TEXT("Snake game v.1.0") );
-	
-	int x = 10;
-	int y = 5;	
-	int color = 10;
-	string chh = "#";
-	writeStrPos(x, y, chh, color);
+	SetConsoleTitle(TEXT("Snake"));
 }
 
-void ConsoleWindowManager::writeStrPos(int & x, int & y, string & str, int & color)
+void ConsoleWindowManager::initLayer()
+{
+	int pos = (LEVEL_LEFT + (LEVEL_WIDTH / 2)) - (GAME_NAME.length() / 2);
+	writeStrPos(pos, 0, GAME_NAME, DEFAULT_CONSOLE_COLOR);
+
+	for (int i = 0; i != LEVEL_WIDTH + 1; i++) {
+		writeStrPos(LEVEL_LEFT + i, LEVEL_TOP, HORIZONTAL_LEVEL_WALL, DEFAULT_CONSOLE_COLOR);
+		writeStrPos(LEVEL_LEFT + i, LEVEL_TOP + LEVEL_HEIGHT, HORIZONTAL_LEVEL_WALL, DEFAULT_CONSOLE_COLOR);
+	}
+	for (int i = 1; i != LEVEL_HEIGHT; i++) {
+		writeStrPos(LEVEL_LEFT, LEVEL_TOP + i, VERTICAL_LEVEL_WALL, DEFAULT_CONSOLE_COLOR);
+		writeStrPos(LEVEL_LEFT + LEVEL_WIDTH, LEVEL_TOP + i, VERTICAL_LEVEL_WALL, DEFAULT_CONSOLE_COLOR);
+	}	
+}
+
+void ConsoleWindowManager::initGame()
+{
+	initStartPos();	
+	gameLoop();
+}
+
+void ConsoleWindowManager::initStartPos()
+{		
+	mSnakeDirection = getRandomNumber(0, 3);
+	int startX = getRandomNumber(LEVEL_LEFT + 1, LEVEL_LEFT + LEVEL_WIDTH - 1);
+	int startY = getRandomNumber(LEVEL_TOP + 1, LEVEL_TOP + LEVEL_HEIGHT - 1);
+	mSnake.push_back(make_tuple(startX, startY));		
+}
+
+int ConsoleWindowManager::gameLoop()
+{	
+	while (true) {
+		if (isKeydown(VK_UP))
+		{
+			mSnakeDirection = 0;
+		}
+		else if (isKeydown(VK_DOWN)) {
+			mSnakeDirection = 1;
+		}
+		else if (isKeydown(VK_LEFT)) {
+			mSnakeDirection = 2;
+		}
+		else if (isKeydown(VK_RIGHT)) {
+			mSnakeDirection = 3;
+		}
+		else if (isKeydown(VK_ESCAPE)) {
+			return 0;
+		}
+		
+		snakeCoord snakeHead = mSnake.at(0);
+
+		switch (mSnakeDirection) {
+			case 0: get<1>(snakeHead)--; break;
+			case 1: get<1>(snakeHead)++; break;
+			case 2: get<0>(snakeHead)--; break;
+			case 3: get<0>(snakeHead)++; break;
+		}
+		mSnake.insert(mSnake.begin(), snakeHead);
+		writeSnake();
+		if (isWallCollision(snakeHead)) {
+			return 0;
+		}
+		//this_thread::sleep_for(std::chrono::seconds(1));
+		Sleep(100);
+	}	
+}
+
+bool ConsoleWindowManager::isWallCollision(const snakeCoord& headCoord)
+{
+	switch (mSnakeDirection) {
+		case 0:  
+			if (get<1>(headCoord) <= LEVEL_TOP) {
+				return true;
+			}
+			break;
+		case 1: 
+			if (get<1>(headCoord) >= LEVEL_TOP + LEVEL_HEIGHT) {
+				return true;
+			}
+			break;
+		case 2: 
+			if (get<0>(headCoord) <= LEVEL_LEFT) {
+				return true;
+			}
+			break;
+		case 3: 
+			if (get<0>(headCoord) >= LEVEL_LEFT + LEVEL_WIDTH) {
+				return true;
+			}
+			break;
+	}
+	return false;
+}
+
+void ConsoleWindowManager::writeSnake()
+{
+	if (!mSnake.empty()) {
+		for each (snakeCoord coord in mSnake)
+		{
+			writeStrPos(get<0>(coord), get<1>(coord), SNAKE_ELEMENT, DEFAULT_CONSOLE_COLOR);
+		}
+	}
+}
+
+void ConsoleWindowManager::writeStrPos(const int & x, const int & y, const string & str, const int & color)
 {
 	mCoord.X = x;
 	mCoord.Y = y;
 	SetConsoleCursorPosition(mHConsole, mCoord);
 	SetConsoleTextAttribute(mHConsole, color);
 	cout << str << endl;
+}
+
+int ConsoleWindowManager::getRandomNumber(const int & min, const int & max)
+{
+	//http://en.cppreference.com/w/cpp/numeric/random/random_device
+	random_device rd;
+	uniform_int_distribution<int> dist(min, max);
+	return dist(rd);	
+}
+
+bool ConsoleWindowManager::isKeydown(const int & key)
+{
+	return (GetAsyncKeyState(key) & 0x8000) != 0;
 }
--- cg/consoleWindowManager.h
+++ cg/consoleWindowManager.h
@@ -5,29 +5,57 @@
 #include <windows.h>
 #include <string>
 #include <tchar.h>
+#include <random>
+#include <tuple>
+#include <vector>
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
 class ConsoleWindowManager {	
-
-	static const bool IS_CENTER_SCREEN = true;
+	
 	static const int CONSOLE_WINDOW_WIDTH = 1000;
 	static const int CONSOLE_WINDOW_HEIGHT = 600;
-	static const int DEFAULT_CONSOLE_COLOR = 10;
+	
+	const string GAME_NAME = "Snake Game v.: 1.0";	
+	const bool IS_CENTER_SCREEN = true;	
+	const string HORIZONTAL_LEVEL_WALL = "-";
+	const string VERTICAL_LEVEL_WALL = "|";	
+	const int DEFAULT_CONSOLE_COLOR = 10;
 
-private:	
+	const int LEVEL_TOP = 2;
+	const int LEVEL_LEFT = 10;
+	const int LEVEL_WIDTH = 100;
+	const int LEVEL_HEIGHT = 30;	
+
+	const string SNAKE_ELEMENT = "#";
+
+	typedef tuple<int, int> snakeCoord;				//http://www.oracle.com/technetwork/articles/servers-storage-dev/c-array-containers-2252536.html
+	
+private:
 	HANDLE mHConsole;
 	HWND mhWnd;
 	RECT mRect;
 	COORD mCoord;
 	int mScreenWidth;
 	int mScreenHeight;
+	int mSnakeDirection;
+	vector <snakeCoord> mSnake;
+	void init(int&, int&);
+	void initStartPos();
+	void initLayer();
+	void initGame();
+	void writeSnake();
+	int gameLoop();
+	void writeStrPos(const int& x, const int& y, const string& ch, const int& color);
+	int getRandomNumber(const int& min, const int& max);
+	bool isKeydown(const int& key);
+	bool isWallCollision(const snakeCoord&);
 
 public:
 	ConsoleWindowManager();
-	~ConsoleWindowManager();
-	void init(int&, int&);
-	void writeStrPos(int& x, int& y, string & ch, int & color);
+	~ConsoleWindowManager();	
 };
 
 #endif // ! CONSOLEMANAGER_H
